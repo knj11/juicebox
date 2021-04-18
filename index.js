d2404a5a977b731680e3f8fa3b8cf735476027c2
Koby Johnson <knj11@vt.edu>
2021-04-17 22:29:11 -0400

    Adding tree of routes through api and router

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ const PORT = 3000;
 const express = require('express');
 const server = express();
 
-server.use((req, res, next) => {
-  console.log("<____Body Logger START____>");
-  console.log(req.body);
-  console.log("<_____Body Logger END_____>");
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
 
-  next();
-});
 
 server.listen(PORT, () => {
   console.log('The server is up on port', PORT)
