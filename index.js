fb3a339eec4737bc18d373154475d60944eab51a
Koby Johnson <knj11@vt.edu>
2021-04-17 22:43:42 -0400

    Adding json and morgan middleWare

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const PORT = 3000;
 const express = require('express');
 const server = express();
 
+
+server.use(express.json());
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
 const apiRouter = require('./api');
 server.use('/api', apiRouter);
 
