8bfea928f9ed4bd4571a437d100db47bca79e46a
Koby Johnson <knj11@vt.edu>
2021-04-20 18:44:40 -0400

    Adding getAllPosts api endpoint

--- api/index.js
+++ api/index.js
@@ -5,4 +5,7 @@ const apiRouter = express.Router();
 const usersRouter = require('./users');
 apiRouter.use('/users', usersRouter);
 
+const postsRouter = require('./posts')
+apiRouter.use('/posts', postsRouter)
+
 module.exports = apiRouter;
---
+++ api/posts.js
@@ -0,0 +1,23 @@
+// api/posts.js
+const express = require('express');
+const getAllPosts = require('../db/posts/getAllPosts');
+
+const postsRouter = express.Router();
+
+
+postsRouter.use((req, res, next) => {
+  console.log("A request is being made to /posts");
+
+  next()
+});
+
+postsRouter.get('/', async (req, res) => {
+  const posts = await getAllPosts();
+
+  res.send({
+    posts
+  });
+ 
+});
+
+module.exports = postsRouter;
