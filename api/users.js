2d1c1ac9398fc6e2d8228d4d929c110e895872ca
Koby Johnson <knj11@vt.edu>
2021-04-21 20:31:42 -0400

    Adding a post handler route for logins

--- api/users.js
+++ api/users.js
@@ -1,6 +1,7 @@
 // api/users.js
 const express = require('express');
 const getAllUsers = require('../db/users/getAllUsers.js');
+const getUserByUsername = require('../db/users/getUserByUsername.js');
 
 const usersRouter = express.Router();
 
@@ -18,6 +19,35 @@ usersRouter.get('/', async (req, res) => {
     users
   });
  
+});
+
+usersRouter.post('/login', async (req, res, next) => {
+  const { username, password } = req.body;
+
+  // request must have both
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password"
+    });
+  }
+
+  try {
+    const user = await getUserByUsername(username);
+
+    if (user && user.password == password) {
+      // create token & return to user
+      res.send({ message: "you're logged in!" });
+    } else {
+      next({ 
+        name: 'IncorrectCredentialsError', 
+        message: 'Username or password is incorrect'
+      });
+    }
+  } catch(error) {
+    console.log(error);
+    next(error);
+  }
 });
 
 module.exports = usersRouter;
