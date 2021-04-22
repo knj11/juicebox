feffd72b5350ace1f207f7e1f8b16521822594e9
Koby Johnson <knj11@vt.edu>
2021-04-21 20:31:05 -0400

    Adding a new DB query function getUserByUsername

---
+++ db/users/getUserByUsername.js
@@ -0,0 +1,17 @@
+const client = require("../client.js")
+
+async function getUserByUsername(username) {
+  try {
+    const { rows: [user] } = await client.query(`
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `, [username]);
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = getUserByUsername;
