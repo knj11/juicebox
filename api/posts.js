ff8c43e23d5f77aeb841085671d018b68fa51a2f
Koby Johnson <knj11@vt.edu>
2021-04-24 12:13:44 -0400

    Adding a create post route

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,8 @@
 // api/posts.js
 const express = require('express');
 const getAllPosts = require('../db/posts/getAllPosts');
+const { requireUser } = require('./utils');
+const createPost = require('../db/posts/createPost')
 
 const postsRouter = express.Router();
 
@@ -17,7 +19,39 @@ postsRouter.get('/', async (req, res) => {
   res.send({
     posts
   });
- 
+
+});
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+  //id is found on the user obj but we rename the var to authorId
+  const { id: authorId } = req.user;
+  const tagArr = tags.trim().split(/\s+/)
+  const postData = {};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    // add authorId, title, content to postData object
+    postData.authorId = authorId
+    postData.title = title
+    postData.content = content
+
+    const post = await createPost(postData);
+    // this will create the post and the tags for us
+    if (post) {
+      res.send({ post })
+    } else {
+      next(new Error('Could not create new post'))
+    }
+    // if the post comes back, res.send({ post });
+    // otherwise, next an appropriate error object 
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = postsRouter;
