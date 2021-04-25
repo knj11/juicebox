2443dd3a4885d0369e7d9dcbbdf1b0c234c77e65
Koby Johnson <knj11@vt.edu>
2021-04-25 18:01:28 -0400

    Adding getPostsByTagName route

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,7 @@
 // api/tags.js
 const express = require('express');
 const getAllTags = require('../db/posts/getAllTags.js');
+const getPostsByTagName = require('../db/posts/getPostsByTagName.js');
 
 const tagsRouter = express.Router();
 
@@ -17,7 +18,22 @@ tagsRouter.get('/', async (req, res) => {
   res.send({
     tags
   });
- 
+
+});
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  // read the tagname from the params
+  const tagname = req.params.tagName
+
+  try {
+    // use our method to get posts by tag name from the db
+    const posts = await getPostsByTagName(tagname)
+    // send out an object to the client { posts: // the posts }
+    res.send({ posts: posts })
+  } catch ({ name, message }) {
+    // forward the name and message to the error handler
+    next({ name, message })
+  }
 });
 
 module.exports = tagsRouter;
